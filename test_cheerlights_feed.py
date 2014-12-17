f3d2946b9c2fd7a12046d33e81947cb38725a946
David Bradway <david.bradway@gmail.com>
2014-12-16 20:30:47 -0500

    remove loop from test

--- test_cheerlights_feed.py
+++ test_cheerlights_feed.py
@@ -12,38 +12,35 @@ cheerlights = color
 
 # Main program logic follows:
 if __name__ == '__main__':
-
-    print 'Press Ctrl-C to quit.'
-    while True:
-        # Read the thingspeak feed to get the current color
-        try:
-            cheerlights = requests.get('http://api.thingspeak.com/channels/1417/field/1/last.json').json['field1']
-        except:
-            pass
-        if cheerlights != color:
-            #New color, do stuff
-            if cheerlights == 'red':
-                print 'match'
-            elif cheerlights == 'green':
-                print 'match'
-            elif cheerlights == 'blue':
-                print 'match'
-            elif cheerlights == 'purple':
-                print 'match'
-            elif cheerlights == 'cyan':
-                print 'match'
-            elif cheerlights == 'magenta':
-                print 'match'
-            elif cheerlights == 'yellow':
-                print 'match'
-            elif cheerlights == 'orange':
-                print 'match'
-            elif (cheerlights == 'white' or cheerlights == 'warmwhite'):
-                print 'match'
-            elif (cheerlights == 'black' or cheerlights == 'off'):
-                print 'match'
-            else:
-                print 'non-match!'
-            print cheerlights
-            color = cheerlights
-        time.sleep(0.1)
+    # Read the thingspeak feed to get the current color
+    try:
+        cheerlights = requests.get('http://api.thingspeak.com/channels/1417/field/1/last.json').json['field1']
+    except:
+        pass
+    if cheerlights != color:
+        #New color, do stuff
+        if cheerlights == 'red':
+            print 'match'
+        elif cheerlights == 'green':
+            print 'match'
+        elif cheerlights == 'blue':
+            print 'match'
+        elif cheerlights == 'purple':
+            print 'match'
+        elif cheerlights == 'cyan':
+            print 'match'
+        elif cheerlights == 'magenta':
+            print 'match'
+        elif cheerlights == 'yellow':
+            print 'match'
+        elif cheerlights == 'orange':
+            print 'match'
+        elif (cheerlights == 'white' or cheerlights == 'warmwhite'):
+            print 'match'
+        elif (cheerlights == 'black' or cheerlights == 'off'):
+            print 'match'
+        else:
+            print 'non-match!'
+        print cheerlights
+        color = cheerlights
+    time.sleep(0.1)
